d59003dc4793726828fac5b1d2710573c603c847
phanhao205 <haophan2052002@gmail.com>
2024-02-21 10:10:45 +0700

    update

--- project-swp/src/App.js
+++ project-swp/src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Homepage from './pages/Homepage/Homepage';
-
+import React, { useState } from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import QuotationForm from "./Pages/QuotationPage/QuotationForm";
+import Projects from "./Pages/ProjectPage/projectsData";
 
 function App() {
   return (
     <div className="App">
-
       <Routes>
-       <Route path="/" element={<Homepage/>}></Route> 
+        <Route path="/" element={<QuotationForm />}></Route>
+        <Route path="/" element={<Projects />}></Route>
       </Routes>
     </div>
   );
 }
+export default App;
