0c782728ef666ef99512cef00021175d370f6bf3
phanhao205 <haophan2052002@gmail.com>
2024-02-24 10:43:05 +0700

    update

--- project-swp/src/App.js
+++ project-swp/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
-import Homepage from "./pages/Homepage/Homepage";
+import Homepage from "./Pages/Homepage/Homepage";
+import FotgtPassword from "./Pages/ForgotPassword/ForgotPassword";
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Routes>
+        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/forgotpassword" element={<FotgtPassword />} />
+      </Routes>
     </div>
   );
 }
