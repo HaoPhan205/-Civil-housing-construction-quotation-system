ebd1ec79160a67a96b78e3d10d1988ae1df60532
phanhao205 <haophan2052002@gmail.com>
2024-02-28 08:49:56 +0700

    update

--- project-swp/src/Pages/SignInPage/SignInPage.js
+++ project-swp/src/Pages/SignInPage/SignInPage.js
@@ -6,6 +6,8 @@ import { useState } from "react";
 
 import user_icon from "../../assets/person.png";
 import password_icon from "../../assets/password.png";
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
 
 function SignInPage() {
   const [username, setName] = useState("");
@@ -31,45 +33,49 @@ function SignInPage() {
       });
   };
   return (
-    <div className="container-signup">
-      <div className="signin">
-        <div className="header-signup">
-          <div className="text">Đăng nhập</div>
-          <div className="underline"></div>
-        </div>
-        <form onSubmit={handleSubmit}>
-          <div className="inputs">
-            <div className="input">
-              <img src={user_icon} alt="user" />
-              <input
-                type="text"
-                placeholder="Username"
-                value={username}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div className="input">
-              <img src={password_icon} alt="password" />
-              <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
+    <div>
+      <Header />
+      <div className="container-signup">
+        <div className="signin">
+          <div className="header-signup">
+            <div className="text">Đăng nhập</div>
+            <div className="underline"></div>
           </div>
+          <form onSubmit={handleSubmit}>
+            <div className="inputs">
+              <div className="input">
+                <img src={user_icon} alt="user" />
+                <input
+                  type="text"
+                  placeholder="Username"
+                  value={username}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="input">
+                <img src={password_icon} alt="password" />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
 
-          <div className="submit-container">
-            <div className="submit">
-              <button type="submit" className="submit-button">
-                Đăng nhập
-              </button>
+            <div className="submit-container">
+              <div className="submit">
+                <button type="submit" className="submit-button">
+                  Đăng nhập
+                </button>
+              </div>
             </div>
-          </div>
-        </form>
-        <p>Chưa có tài khoản?</p>
-        <Link to="/signup">Đăng ký</Link>
+          </form>
+          <p>Chưa có tài khoản?</p>
+          <Link to="/signup">Đăng ký</Link>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
--- project-swp/src/Pages/SignUpPage/SignUpPage.js
+++ project-swp/src/Pages/SignUpPage/SignUpPage.js
@@ -3,18 +3,20 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import "./SignUpPage.css";
 
-import backgroundImage from "../../assets/signinup_background.png"
+import backgroundImage from "../../assets/signinup_background.png";
 import user_icon from "../../assets/person.png";
 import email_icon from "../../assets/email.png";
 import password_icon from "../../assets/password.png";
 import axios from "axios";
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
 
 function SignUpPage() {
   const backgroundStyle = {
     backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat'
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
   };
 
   const [username, setName] = useState("");
@@ -40,59 +42,63 @@ function SignUpPage() {
   };
 
   return (
-    <div style={backgroundStyle}>
-      <div className="container-signup">
-        <div className="signup">
-          <div className="header-signup">
-            <div className="text">Đăng Ký</div>
-            <div className="underline"></div>
+    <div>
+      <Header />
+      <div style={backgroundStyle}>
+        <div className="container-signup">
+          <div className="signup">
+            <div className="header-signup">
+              <div className="text">Đăng Ký</div>
+              <div className="underline"></div>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="inputs">
+                <div className="input">
+                  <img src={user_icon} alt="user" />
+                  <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className="input">
+                  <img src={email_icon} alt="email" />
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="input">
+                  <img src={password_icon} alt="password" />
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="signInChoice">
+                <p className="signInChoiceText">Đã có tài khoản?</p>
+                <Link to="/signin" className="signInChoiceButton">
+                  Đăng nhập
+                </Link>
+              </div>
+              <div className="submit-container">
+                <div className="submit">
+                  <button type="submit" className="submit-button">
+                    Đăng Ký
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
-          <form onSubmit={handleSubmit}>
-            <div className="inputs">
-              <div className="input">
-                <img src={user_icon} alt="user" />
-                <input
-                  type="text"
-                  placeholder="Username"
-                  value={username}
-                  onChange={(e) => setName(e.target.value)}
-
-                />
-              </div>
-              <div className="input">
-                <img src={email_icon} alt="email" />
-                <input
-                  type="email"
-                  placeholder="Email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-              <div className="input">
-                <img src={password_icon} alt="password" />
-                <input
-                  type="password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-
-            </div>
-            <div className="signInChoice">
-              <p className="signInChoiceText">Đã có tài khoản?</p>
-              <Link to="/signin" className="signInChoiceButton">Đăng nhập</Link>
-            </div>
-            <div className="submit-container">
-              <div className="submit">
-                <button type="submit" className="submit-button">
-                  Đăng Ký
-                </button>
-              </div>
-            </div>
-          </form>
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
