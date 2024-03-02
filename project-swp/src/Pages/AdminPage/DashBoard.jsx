abf2e674a499213b1a1aae7799d8f418b721f3d3
phanhao205 <haophan2052002@gmail.com>
2024-03-02 10:11:14 +0700

    update

--- project-swp/src/Pages/AdminPage/DashBoard.jsx
+++ project-swp/src/Pages/AdminPage/DashBoard.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./DashBoard.css";
 import Sidebar from "./components/Sidebar";
-import MainDash from "./components/MainDash";
-import RightSide from "./components/RigtSide";
+import MainDash from "./components/MainDash/MainDash";
+import RightSide from "./components/RigtSide/RightSide";
 
 export default function DashBoard() {
   return (
--- project-swp/src/Pages/Homepage/Homepage.js
+++ project-swp/src/Pages/Homepage/Homepage.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
-//import Footer from '../../footer/Footer'
 import Carousel from "../../Components/carousel/Carousel";
 import "./Homepage.css";
 import ContentUpperHomepage from "../../Components/contentUpperHomepage/ContentUpperHomepage";
 import ContentMiddleHomepage from "../../Components/contentMiddleHomepage/ContentMiddleHomepage";
 import ContentLowerHomepage from "../../Components/contentLowerHomepage.js/ContentLowerHomepage";
 import Footer from "../../Components/footer/Footer";
+
 export default function Homepage() {
   return (
     <div className="homepage">
