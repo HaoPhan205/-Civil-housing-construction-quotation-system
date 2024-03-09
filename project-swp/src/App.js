6d5281f23f408244c4331337e523b7a0d28ffdac
Gia Khang <khanggia85@gmail.com>
2024-03-09 11:33:35 +0700

    updated app.js

--- project-swp/src/App.js
+++ project-swp/src/App.js
@@ -1,4 +1,6 @@
-import React from "react";
+/** @format */
+
+import React, { createContext, useState } from "react";
 import "./App.css";
 import Homepage from "./Pages/Homepage/Homepage";
 import FotgtPassword from "./Pages/ForgotPassword/ForgotPassword";
@@ -7,18 +9,44 @@ import SignUpPage from "./Pages/SignUpPage/SignUpPage";
 import SignInPage from "./Pages/SignInPage/SignInPage";
 import Quotation from "./Pages/QuotationPage/baogia";
 import Contact from "./Pages/Contact/Contact";
+import { useStorage } from "./Services/Hooks/useStorage";
 
+export const Data = createContext(null);
 function App() {
+  const { getFromStorage } = useStorage();
+  const [user, setUser] = useState(
+    getFromStorage("user") ? getFromStorage("user") : null
+  );
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/forgotpassword" element={<FotgtPassword />} />
-        <Route path="/signin" element={<SignInPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/quotation" element={<Quotation />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <Data.Provider value={{ user, setUser }}>
+        <Routes>
+          <Route
+            path="/"
+            element={<Homepage />}
+          />
+          <Route
+            path="/forgotpassword"
+            element={<FotgtPassword />}
+          />
+          <Route
+            path="/signin"
+            element={<SignInPage />}
+          />
+          <Route
+            path="/signup"
+            element={<SignUpPage />}
+          />
+          <Route
+            path="/quotation"
+            element={<Quotation />}
+          />
+          <Route
+            path="/contact"
+            element={<Contact />}
+          />
+        </Routes>
+      </Data.Provider>
     </div>
   );
 }
